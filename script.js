2b385fd397d0d8b8b921564cbc14da31eca8c5a2
GreetyCr <randallgbc@gmail.com>
2023-06-19 16:58:11 -0600

    Add open and close functions
    Add Javascript Functions
    Fix navbar issues

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  document.querySelector('.menu__backdrop').style.display = 'flex';
+}
+
+function closeMenu() {
+  document.querySelector('.menu__backdrop').style.display = 'none';
+}
