2e71b8c4201aed2622344b61f697773bff3a755a
GreetyCr <randallgbc@gmail.com>
2023-06-19 17:15:01 -0600

    Fix js linter

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function openMenu() {
 function closeMenu() {
   document.querySelector('.menu__backdrop').style.display = 'none';
 }
+
+const btnMenu = document.getElementById('btnMenu');
+const btnMenu2 = document.getElementById('btnMenu2'); 
+
+btnMenu.onclick = () => {
+  openMenu();
+}
+
+btnMenu2.onclick = () => {
+  closeMenu();
+}
